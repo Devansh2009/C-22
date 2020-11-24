9d1601f1291e88f00c634742358936fa4855e40e
Devansh2009 <73864262+Devansh2009@users.noreply.github.com>
2020-11-23 19:14:32 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+
+function setup(){
+    var canvas = createCanvas(800,800);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,100,50,50);
+    box2 = new Box(220,50,50,80);
+    ground = new Ground(400,780,800,20);
+    
+
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+
+}
